aa7a8739d2d60ba270db2cd7aabf3bcf9c63c245
Hunghh.Dev <hunghh.dev@gmail.com>
2020-04-14 22:53:14 +0700

    setup vuex :fire:

---
+++ srr/nuxt-blog/store/modules/post.js
@@ -0,0 +1,15 @@
+const state = () => ({
+    posts:[{id: 1, title: "hello nuxt"}]
+})
+const getters = {};
+const actions = {};
+const mutations = {};
+
+const postModule = {
+    state,
+    getters,
+    actions,
+    mutations
+}
+
+export default postModule
